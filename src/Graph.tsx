dd6d3cfda2c2741940e957e0370f59b2bebfeead
sdutta2313 <82588975+sdutta2313@users.noreply.github.com>
2023-03-11 20:35:10 +0530

    Add files via upload

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -47,7 +47,16 @@ class Graph extends Component<IProps, {}> {
     if (this.table) {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
 
-      // Add more Perspective configurations here.
+      elem.setAttribute('view','y_line');
+      elem.setAttribute('column-pivots','["stock"]');
+      elem.setAttribute('row-pivot','["timestamp"]');
+      elem.setAttribute('columns','["top_ask_price"]');
+      elem.setAttribute('aggregates',`
+        {"stock":"distinct count",
+        "top_ask_price":"avg",
+        "top_bid_price":"avg",
+        "timestamp":"distinct count"}`);
+
       elem.load(this.table);
     }
   }
